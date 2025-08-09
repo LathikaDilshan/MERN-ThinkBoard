97cce1c7915567db14fec89e598d621d3fc9bad4
Lathika Dilshan <133035674+LathikaDilshan@users.noreply.github.com>
2025-08-10 00:55:01 +0530

    navbar
    successfully created navbar inside main page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useState } from 'react';
+import Navbar from '../components/Navbar'
+import RateLimitedUi from '../components/RateLimitedUi';
+import { Turtle } from 'lucide-react';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true);
   return (
-    <div>
-      home
+    <div className="min-h-screen">
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUi/>}
     </div>
   )
 }
