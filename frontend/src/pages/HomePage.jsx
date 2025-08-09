878e837baed170d71b7f0c84ce41916d4e4e9d39
Lathika Dilshan <133035674+LathikaDilshan@users.noreply.github.com>
2025-08-10 02:05:12 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 import Navbar from '../components/Navbar'
 import RateLimitedUi from '../components/RateLimitedUi';
 import { Turtle } from 'lucide-react';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]); 
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try{
+        const res = await axios.post("http://localhost:5001/api/notes");
+        console.log(res.data);
+      } catch(error){
+        console.error("Error fetching notes:", error);
+      }
+    };
+
+    fetchNotes();
+  }, []);
+  
   return (
     <div className="min-h-screen">
       <Navbar />
